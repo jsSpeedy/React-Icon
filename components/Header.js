6f410e05b34ac7a825ef42e8843d32eecc32d763
Shifu <aemre3113@gmail.com>
2023-11-17 16:02:28 +0300

    CSS düzenlemesi yapıldı

--- components/Header.js
+++ components/Header.js
@@ -2,23 +2,31 @@ import Button from "./Button";
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
+  background-color: #000;
+`;
+
+const HeaderInner = styled.div`
+  margin: auto;
+  max-width: 800px;
   padding: 30px;
   display: flex;
   gap: 20px;
-  background-color: #000;
 `;
+
 export default () => (
   <HeaderDiv>
-    <Button href="/" passHref>
-      Anasayfa
-    </Button>
+    <HeaderInner>
+      <Button href="/" passHref>
+        Anasayfa
+      </Button>
 
-    <Button href="/about" passHref>
-      Hakkımda
-    </Button>
+      <Button href="/about" passHref>
+        Hakkımda
+      </Button>
 
-    <Button href="/blog" passHref>
-      Blog
-    </Button>
+      <Button href="/blog" passHref>
+        Blog
+      </Button>
+    </HeaderInner>
   </HeaderDiv>
 );
--- components/Layout.js
+++ components/Layout.js
@@ -1,35 +1,49 @@
 import Header from "./Header";
 import styled from "styled-components";
 
+const Content = styled.div`
+  max-width: 800px;
+  margin: auto;
+`;
+
+const ContentInner = styled.div`
+  padding: 20px;
+`;
+
 const H1 = styled.h1`
   padding: 20px;
 `;
 
 const Footer = styled.footer`
-  padding: 20px;
   background: #000;
   position: fixed;
   bottom: 0;
   width: 100%;
 `;
 
-const Content = styled.div`
+const FooterInner = styled.div`
   padding: 20px;
+  max-width: 800px;
+  margin: auto;
 `;
 
 export default ({ children, title }) => (
   <div>
     <Header />
-    <H1>{title}</H1>
-    <Content>{children}</Content>
+    <Content>
+      <H1>{title}</H1>
+      <ContentInner>{children}</ContentInner>
+    </Content>
     <Footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by vercel
-      </a>
+      <FooterInner>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by vercel
+        </a>
+      </FooterInner>
     </Footer>
   </div>
 );
