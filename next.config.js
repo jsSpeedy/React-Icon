3162079af9b46132dee051ee350f4a36c762b296
Shifu <aemre3113@gmail.com>
2024-04-17 15:35:48 +0300

    nextConfig dosyası optimizasyon yapıldı.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  output: "export",
-  reactStrictMode: true,
+  //output: "export",
+  // reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media.amazon.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
